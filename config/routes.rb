6cf008518e613fb87ae02592121176440f533e96
kamonaoki <naokun616@gmail.com>
2024-06-28 18:10:19 +0900

    地図が表示できるようにAPI導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ resources :posts, only: [:index, :new, :create]
+  root 'posts#index'
 end
